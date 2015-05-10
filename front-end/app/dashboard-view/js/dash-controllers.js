690d407d1edec40d1b710c52be35d325e4082f0c
Oscar De Ángel <deangel95@gmail.com>
2015-05-09 23:07:11 -0500

    Search filters fixed

--- front-end/app/dashboard-view/js/dash-controllers.js
+++ front-end/app/dashboard-view/js/dash-controllers.js
@@ -4,24 +4,52 @@ var dashControllers = angular.module('dashControllers', [])
 
 .controller('DashViewCtrl', ['$scope', 'CategoryRequest', 'DoubtRequest',
 	function($scope, CategoryRequest, DoubtRequest) {
+		/* Miscellaneous functions */
+		function intersect(x, y){
+			var ret = [];
+			for (var i = 0; i < x.length; i++) {
+				for (var z = 0; z < y.length; z++) {
+					if (x[i] === y[z]) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+
 		//Category banner controller
 		$scope.categories = CategoryRequest.query();
 		$scope.orderCategory = "id";
 		$scope.selectedCategory = null;
 
 		//Search controller
-		$scope.selectedOrder = false;
-		$scope.selectedCriteria = null;
-		$scope.filterByUser = false;
-		$scope.tagLabels = [
-			{
-				'id': 1,
-				'name': 'Tag 1',
-				'included': false
+		//Data query
+		$scope.tagSet = [];
+		$scope.doubtList = DoubtRequest.query();
+		$scope.doubtList.$promise.then(function (data) {
+			for (var i = data.length - 1; i >= 0; i--) {
+				for (var j = data[i].tags.length - 1; j >= 0; j--) {
+					var tag = data[i].tags[j];
+					if ($scope.tagSet.indexOf(tag) === -1) { $scope.tagSet.push(tag); }
+				}
 			}
-		];
+		});
+
+
+		//Filtering and ordering
+		$scope.selectedOrder = 0;
+		$scope.selectedCriteria = 'title';
+		$scope.enteredTags = '';
 		$scope.searchTerm = "";
 
-		$scope.doubtList = DoubtRequest.query();
+		$scope.tagFilter = function(item) {
+			var desiredTags = $scope.enteredTags.split(',').map(function (str) {
+				return str.trim();
+			}).filter(function (value) { return value !== '' });
+			if (desiredTags.length === 0) { return true; }
+			if (intersect(item.tags, desiredTags)) { return true; }
+			return false;
+		};
 	}
 ]);
