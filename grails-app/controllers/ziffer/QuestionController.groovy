e06eb9c1efdfc368158cc71d028a92785013e768
Daniel Aranda <dwares2008@hotmail.com>
2015-05-21 02:28:00 -0500

    Fixed method for create questions and render validation in JSON

--- grails-app/controllers/ziffer/QuestionController.groovy
+++ grails-app/controllers/ziffer/QuestionController.groovy
@@ -11,19 +11,44 @@ class QuestionController {
 
     def create() {
 
+
+	//Pruebas con estos valores, debe moverse a testing luego
+	/*def params = "a,b,c,d,e,f"
+	def title = "blablabla"
+	def description = "blablablbalbalablalba"
+	def date = "21/05/2015"
+	def tags = params.tokenize(',') as Set
+	def asker = User.get(1)
+	def category = Category.get(1)*/
+
+	//Obtiene un set de tags separados por ','	
 	def tags = params.tags.tokenize(',') as Set
-	def asker = User.get(params.asker)
+
+	//Obtener los objetos usuario y categoría para persistirlos con la nueva pregunta
+	def asker = User.get(params.user)
 	def category = Category.get(params.category)
-	Question question = new Question(title: params.title, description: params.description, dueDate: new Date.parse('dd/MM/yyyy', params.date),
+
+	//Asume errores
+	def validation = false
+
+	//Crea la pregunta con los parámetros enviados más los procesados
+	Question question = new Question(title: params.title, description: params.description, dueDate: Date.parse('dd/MM/yyyy', params.date),
 			    asker: asker, category: category, tags: tags)
-	if(question.save()){
+	//Si las validaciones se hicieron
+	if(question.validate()){
+
+		//Añadir la pregunta a las listas
 		asker.addToQuestions(question)
 		category.addToQuestions(question)
 		asker.save()
 		category.save()
-		return true
+		validation = true
 	}
-	return false
+
+	//Construcción del JSON
+	def answer = [:]
+	answer["success"] = validation
+	return answer as JSON
     }
 
 }
