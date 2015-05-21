ca24e1091f497deac9ad5b4d147978a04169ec8d
Daniel Aranda <dwares2008@hotmail.com>
2015-05-21 01:41:26 -0500

    Added create question response

--- grails-app/controllers/ziffer/QuestionController.groovy
+++ grails-app/controllers/ziffer/QuestionController.groovy
@@ -9,4 +9,21 @@ class QuestionController {
         render json
     }
 
+    def create() {
+
+	def tags = params.tags.tokenize(',') as Set
+	def asker = User.get(params.asker)
+	def category = Category.get(params.category)
+	Question question = new Question(title: params.title, description: params.description, dueDate: new Date.parse('dd/MM/yyyy', params.date),
+			    asker: asker, category: category, tags: tags)
+	if(question.save()){
+		asker.addToQuestions(question)
+		category.addToQuestions(question)
+		asker.save()
+		category.save()
+		return true
+	}
+	return false
+    }
+
 }
